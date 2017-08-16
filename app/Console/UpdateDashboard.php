42a92e69c0b7381c283eac77f2479b22b34a7458
Brandon Ferens <brandon@potentialabs.com>
2017-08-15 18:20:30 -0700

    Console command updates

--- app/Console/UpdateDashboard.php
+++ app/Console/UpdateDashboard.php
@@ -12,11 +12,9 @@ class UpdateDashboard extends Command
 
     public function handle()
     {
-        $this->call('dashboard:fetch-github-totals');
         $this->call('dashboard:fetch-calendar-events');
         $this->call('dashboard:send-heartbeat');
         $this->call('dashboard:fetch-current-track');
-        $this->call('dashboard:fetch-packagist-totals');
         $this->call('dashboard:fetch-issues');
     }
 }
