85fd80cdff952e0e788fbcc44a19deaa2e98d5f9
Brandon Ferens <brandon@potentialabs.com>
2017-08-22 10:06:00 -0700

    Added Steph to Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,15 +9,10 @@
     <music position="b1:c1"></music>
     <uptime position="a3"></uptime>
 
-    <!-- <tasks team-member="alex" position="b2"></tasks> -->
-    <!-- <tasks team-member="freek" position="c2"></tasks> -->
-    <!-- <tasks team-member="seb" position="b3"></tasks> -->
-    <!-- <tasks team-member="willem" position="c3"></tasks> -->
-
     <jira team-member="brandon" position="b2"></jira>
     <jira team-member="robin" position="c2"></jira>
     <jira team-member="dustin" position="b3"></jira>
-    <jira team-member="eric" position="c3"></jira>
+    <jira team-member="steph" position="c3"></jira>
 
     <time-weather position="d1" date-format="ddd M/D"></time-weather>
     <time-weather position="d2" date-format="ddd M/D" city="San Diego"></time-weather>
